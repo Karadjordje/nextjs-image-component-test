3c65449b561abb9c676e13855200acab247af2fa
Karadjordje <sveljod@gmail.com>
2020-12-24 20:08:35 +0100

    Some small CSS adjustment

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
+    <div className='container'>
       <Image
         src='/assets/pexels-kendall-hoopes-6000x4000.jpg'
         alt='universe'
         width={1500}
         height={1000}
+        layout={'responsive'}
       />
     </div>
   )
